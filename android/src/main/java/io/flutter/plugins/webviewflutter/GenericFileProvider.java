2bb6e76284bd58aa2f0d2cc4cd2cc1a75f0420af
陈磊的MacBook Pro <200892907@qq.com>
2020-10-31 16:23:57 +0800

    feat:解决file选择

---
+++ android/src/main/java/io/flutter/plugins/webviewflutter/GenericFileProvider.java
@@ -0,0 +1,10 @@
+package io.flutter.plugins.webviewflutter;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * 作者:chenlei
+ * 时间:2020/10/31 3:49 PM
+ */
+public class GenericFileProvider extends FileProvider {
+}
